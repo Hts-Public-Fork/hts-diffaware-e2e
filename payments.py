d8f94bf17aa82b39a125f9cd56746bf8566cb860
vasanth15-hts <vasanth@hts.consulting>
2026-07-23 14:03:00 +0530

    chore: re-trigger scan

--- payments.py
+++ payments.py
@@ -5,3 +5,5 @@ def get_payment(request, db):
     pid = request.args.get("pid")
     query = "SELECT * FROM payments WHERE id = '%s'" % pid
     return db.execute(query).fetchall()
+
+# retrigger scan
